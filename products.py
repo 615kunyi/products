f243633f54c698bfaf62be4093b2ef8ef63030bf
Kun Yi <kunyi1989@gmail.com>
2021-06-23 16:09:19 +0800

    更新

--- products.py
+++ products.py
@@ -1,11 +1,17 @@
-#讀取檔案
+import os
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
+#讀取檔案
+if os.path.isfile('products.csv'):
+	print('正在讀取檔案...')
+	with open('products.csv', 'r', encoding='utf-8') as f:
 	for line in f:
 		if '商品,價格' in line:
 			continue
 		name, price = line.strip().split(',')
 		products.append([name, price])
+else:
+	print('找不到檔案,正在新增資料')
 
 #請使用者輸入 商品及價格
 while True:
